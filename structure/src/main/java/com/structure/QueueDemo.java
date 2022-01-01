75b326e7246def3ffbeb3a339226c1fc3626b06e
Mark Cui <hjom3tp6@gmail.com>
2022-01-01 21:54:19 +0800

    [str]  add queue

--- structure/src/main/java/com/structure/QueueDemo.java
+++ structure/src/main/java/com/structure/QueueDemo.java
@@ -9,54 +9,67 @@ public class QueueDemo {
         queue.insert(3);
         queue.insert(4);
         queue.listQueue();
+        queue.get();
+        queue.get();
+        queue.insert(5);
+        queue.insert(6);
+        queue.listQueue();
     }
 }
 
 class ArrayQueue {
 
-    private int[] queue;
-    private int maxSize;
+    private int[] arr;
+    private int arrSize;
     private int front;
     private int rear;
 
-    public ArrayQueue(int maxSize) {
-        this.maxSize = maxSize +1;
-        this.queue = new int[maxSize];
-        this.front = 0;
-        this.rear = 0;
+    public ArrayQueue(int queueSize) {
+        arrSize = queueSize + 1;
+        arr = new int[arrSize];
+        front = 0;
+        rear = 0;
     }
 
     public void insert(int value) {
-        if (this.isFull()) {
+        if (isFull()) {
             System.out.println(" Is full !");
         } else {
-            this.queue[this.rear] = value;
-            this.rear = (this.rear + 1) % this.maxSize;
+            arr[rear] = value;
+            rear = (rear + 1) % arrSize;
         }
     }
 
     public int get() {
-        if (this.isEmpty()) {
-            System.out.println(" Is empty !");
-            return -1;
+        if (isEmpty()) {
+            throw new RuntimeException("Is empty !");
         }
-        int value = this.queue[this.front];
-        this.front = (this.front + 1) % this.maxSize;
+        int value = arr[front];
+        front = (front + 1) % arrSize;
         return value;
     }
 
     public boolean isFull() {
-        return this.maxSize ==  (this.rear + 1) %  this.maxSize;
+        return front == (rear + 1) % arrSize;
     }
 
     public boolean isEmpty() {
-        return this.front == this.rear;
+        return front == rear;
     }
 
     public void listQueue() {
-        for (int i : this.queue) {
-            System.out.println(i);
+        if (isEmpty()) {
+            System.out.println("Is empty !");
+        } else {
+            for (int i = front; i < front + size(); i++) {
+                System.out.print(arr[i % arrSize] + " ");
+            }
+            System.out.println();
         }
+    }
+
+    public int size() {
+        return (rear - front + arrSize) % arrSize;
     }
 
 }
