4c0e0c494934d9c05ec7b13d6a2308251a9e374e
DESKTOP-7MPAJV0\user <hjom3tp6@gmail.com>
2022-01-13 23:57:29 +0800

    [DoubleLinkedList] list & addByOrder

---
+++ structure/src/test/java/com/structure/linkedlist/DoubleLinkedListTest.java
@@ -0,0 +1,31 @@
+package com.structure.linkedlist;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DoubleLinkedListTest {
+
+    DoubleLinkedList list = new DoubleLinkedList();
+
+    @Before
+    public void setUp() throws Exception {
+        DoubleNode node1 = new DoubleNode(1, "node1");
+        DoubleNode node2 = new DoubleNode(2, "node2");
+        DoubleNode node3 = new DoubleNode(4, "node4");
+        list.addByOrder(node1);
+        list.addByOrder(node2);
+        list.addByOrder(node3);
+        list.listAll();
+    }
+
+    @Test
+    public void addTest() {
+        System.out.println("========addByOrder========");
+        DoubleNode nodeInsert = new DoubleNode(3, "nodeInsert");
+        list.addByOrder(nodeInsert);
+        list.listAll();
+        System.out.println("========listFromTail========");
+        list.listFromTail();
+    }
+    
+}
