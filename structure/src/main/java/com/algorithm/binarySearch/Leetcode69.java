91fb33576e605947cb15d8e4fc1f16fd4c4f8a77
Mark Cui <hjom3tp6@gmail.com>
2022-04-08 18:57:50 +0800

    [binary search] leetcode 69

---
+++ structure/src/main/java/com/algorithm/binarySearch/Leetcode69.java
@@ -0,0 +1,28 @@
+package com.algorithm.binarySearch;
+
+public class Leetcode69 {
+
+  public static void main(String[] args) {
+    // 0 <= x <= 2^31 - 1
+    int sqrt4 = sqrt(4); // 2
+    System.out.println(sqrt4);
+    int sqrt8 = sqrt(8); // 2
+    System.out.println(sqrt8);
+  }
+
+  public static int sqrt(int num) {
+
+    // x * x <= num
+    int left = 0;
+    int right = num;
+    while (left < right) {
+      int mid = (left + right + 1) >> 1;
+      if (mid * mid <= num) {
+        left = mid;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return right;
+  }
+}
