fbc6e1f6c1626d3266205ff7a293d601da189a12
DESKTOP-7MPAJV0\user <hjom3tp6@gmail.com>
2022-05-04 23:35:42 +0800

    [arrays] leetcode 169

--- structure/src/main/java/com/array/Leetcode_169.java
+++ structure/src/main/java/com/array/Leetcode_169.java
@@ -52,27 +52,19 @@ public class Leetcode_169 {
 
   // HashMap
   public static int majorityElement_HashMap(int[] nums) {
-    Map<Integer, Integer> counts = countNums(nums);
-    Map.Entry<Integer, Integer> majorityEntry = null;
-    for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-      System.out.print(entry.getValue() + " ");
-      if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
-        majorityEntry = entry;
-      }
-    }
-
-    return majorityEntry.getKey();
-  }
-
-  private static Map<Integer, Integer> countNums(int[] nums) {
-    Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
     for (int num : nums) {
-      if (counts.containsKey(num)) {
-        counts.put(num, counts.get(num) + 1);
-      } else {
-        counts.put(num, 1);
-      }
+        if (map.containsKey(num)) {
+            map.put(num, map.get(num) + 1);
+        } else {
+            map.put(num, 1);
+        }
     }
-    return counts;
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        if (entry.getValue() > nums.length / 2) {
+            return entry.getKey();
+        }
+    }
+    return -1;
   }
 }
