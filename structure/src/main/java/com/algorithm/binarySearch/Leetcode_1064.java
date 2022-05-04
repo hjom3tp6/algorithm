6754ae931437ecca99abc14727944b6874ef291a
DESKTOP-7MPAJV0\user <hjom3tp6@gmail.com>
2022-05-04 22:55:55 +0800

    [binary search] leetcode 1064

---
+++ structure/src/main/java/com/algorithm/binarySearch/Leetcode_1064.java
@@ -0,0 +1,16 @@
+package com.algorithm.binarySearch;
+
+public class Leetcode_1064 {
+    public int fixedPoint(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (arr[mid] >= mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return arr[left] == left ? left : -1;
+    }
+}
