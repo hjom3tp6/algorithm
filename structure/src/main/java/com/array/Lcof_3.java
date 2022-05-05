2457578a9b26aa5f28fd268cc482dc6a984dc12a
DESKTOP-7MPAJV0\user <hjom3tp6@gmail.com>
2022-05-06 00:00:27 +0800

    [array] Lcof 3

--- structure/src/main/java/com/array/Lcof_3.java
+++ structure/src/main/java/com/array/Lcof_3.java
@@ -2,13 +2,15 @@ package com.array;
 
 /**
  * https://github.com/doocs/leetcode/blob/v0.1.8/lcof/%E9%9D%A2%E8%AF%95%E9%A2%9803.%20%E6%95%B0%E7%BB%84%E4%B8%AD%E9%87%8D%E5%A4%8D%E7%9A%84%E6%95%B0%E5%AD%97/README.md
+ * leetcode 287
  */
 public class Lcof_3 {
 
     public int findRepeatNumber(int[] nums) {
-        for (int i  = 0; i < nums.length ; i++) {
+        for (int i = 0; i < nums.length; i++) {
             while (nums[i] != i) {
-                if (nums[i] == nums[nums[i]]) return nums[i];
+                if (nums[i] == nums[nums[i]])
+                    return nums[i];
                 int temp = nums[i];
                 nums[i] = nums[nums[i]];
                 nums[nums[i]] = temp;
@@ -16,5 +18,5 @@ public class Lcof_3 {
         }
         return -1;
     }
-    
+
 }
