5edeb28ed098733470cfc7c716ae23610227e68f
DESKTOP-7MPAJV0\user <hjom3tp6@gmail.com>
2022-05-30 22:52:55 +0800

    [Array] Leetcode 15

--- structure/src/main/java/com/array/Leetcode_15.java
+++ structure/src/main/java/com/array/Leetcode_15.java
@@ -19,20 +19,20 @@ public class Leetcode_15 {
       int left = i + 1;
       int right = nums.length - 1;
       while (left < right) {
-        if (nums[i] + nums[left] + nums[right] == 0) {
+        if (nums[left] + nums[right] + nums[i] == 0) {
           res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-          left++;
-          right--;
-          while (left < nums.length && nums[left] == nums[left - 1]) {
+          do {
             left++;
-          }
-          while (right > i && nums[right] == nums[right + 1]) {
+          } while (left < nums.length && nums[left] == nums[left - 1]);
+
+          do {
             right--;
-          }
-        } else if (nums[i] + nums[left] + nums[right] < 0) {
-          left++;
-        } else {
+          } while (right > i && nums[right] == nums[right + 1]);
+
+        } else if (nums[left] + nums[right] + nums[i] > 0) {
           right--;
+        } else {
+          left++;
         }
       }
     }
