1c07b23e5233f30cddb9daa7cca47838d62062c7
TW - IT - Mark Cui <mark.cui@shoalter.com>
2022-08-23 11:12:27 +0800

    [String] Leetcode 49

--- structure/src/main/java/com/algorithm/string/Leetcode_49.java
+++ structure/src/main/java/com/algorithm/string/Leetcode_49.java
@@ -1,13 +1,15 @@
 package com.algorithm.string;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 /**
  * https://leetcode.com/problems/group-anagrams/
- * key point: convert anagram as hashmap key
+ * key point: convert anagram as a hashmap key
  * The method of conversion can be can be int[] or char[]
  */
 public class Leetcode_49 {
@@ -17,7 +19,7 @@ public class Leetcode_49 {
     }
     Map<String, List<String>> map = new HashMap<>();
     for (String s : strs) {
-      // convert anagram as a hashmap key
+      // convert anagram as a hashmap key, use char[] and String.valueOf()
       char[] chars = new char[26];
       for (char c : s.toCharArray()) {
         chars[c - 'a']++;
@@ -27,6 +29,23 @@ public class Leetcode_49 {
         map.put(keyStr, new ArrayList<>());
       }
       map.get(keyStr).add(s);
+    }
+    return new ArrayList<>(map.values());
+  }
+
+  // use int[] and Arrays.toString
+  public List<List<String>> groupAnagrams_intArray(String[] strs) {
+    Map<String, List<String>> map = new HashMap<>();
+    for (String s : strs) {
+      int[] count = new int[26];
+      for (char ch : s.toCharArray()) {
+        count[ch - 'a']++;
+      }
+      String key = Arrays.toString(count);
+      if (!map.containsKey(key)) {
+        map.put(key, new LinkedList<>());
+      }
+      map.get(key).add(s);
     }
     return new ArrayList<>(map.values());
   }
